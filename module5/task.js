2ae5476b05144b3a24f82865beed43b429e4dcbf
Karina-Sapezhko <karina.sapezhko@gmail.com>
2020-07-28 14:07:06 +0300

    HW 5

---
+++ module5/task.js
@@ -0,0 +1,190 @@
+// ------------------------------------------------------#1---------------------------------------
+// const Account = function (user) {
+//   this.login = user.login;
+//   this.email = user.email;
+// };
+// Account.prototype.getInfo = function () {
+//   console.log(`Login: ${this.login}, Email: ${this.email}`);
+// };
+// // console.log(Account);
+// console.log(Account.prototype.getInfo); // function
+
+// const mango = new Account({
+//   login: "Mangozedog",
+//   email: "mango@dog.woof",
+// });
+
+// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+// // console.log(mango.getInfo());
+// const poly = new Account({
+//   login: "Poly",
+//   email: "poly@mail.com",
+// });
+
+// poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+// ------------------------------------------------------#2---------------------------------------
+// class User {
+//   constructor(obj) {
+//     this.name = obj.name;
+//     this.age = obj.age;
+//     this.followers = obj.followers;
+//   }
+
+//   getInfo() {
+//     console.log(
+//       `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
+//     );
+//   }
+// }
+
+// const mango = new User({
+//   name: "Mango",
+//   age: 2,
+//   followers: 20,
+// });
+
+// mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+// const poly = new User({
+//   name: "Poly",
+//   age: 3,
+//   followers: 17,
+// });
+
+// poly.getInfo(); // User Poly is 3 years old and has 17 followers
+
+// ------------------------------------------------------#3---------------------------------------
+
+// class Storage {
+//   constructor(obj) {
+//     this.items = obj;
+//   }
+
+//   getItems() {
+//     return this.items;
+//   }
+//   addItem(item) {
+//     this.items.push(item);
+//   }
+//   removeItem(item) {
+//     this.items.splice(this.items.indexOf(item), 1);
+//   }
+// }
+
+// const storage = new Storage([
+//   "Нанитоиды",
+//   "Пролонгер",
+//   "Железные жупи",
+//   "Антигравитатор",
+// ]);
+
+// const items = storage.getItems();
+// console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+// storage.addItem("Дроид");
+// console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+// storage.removeItem("Пролонгер");
+// console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+// ------------------------------------------------------#4---------------------------------------
+
+// class StringBuilder {
+//   constructor(value) {
+//     this._value = value;
+//   }
+
+//   get value() {
+//     return this._value;
+//   }
+//   append(str) {
+//     return (this._value = this._value + str);
+//   }
+//   prepend(str) {
+//     return (this._value = str + this._value);
+//   }
+//   pad(str) {
+//     return (this._value = str + this._value + str);
+//   }
+// }
+// const builder = new StringBuilder(".");
+// builder.append("^");
+// console.log(builder.value); // '.^'
+
+// builder.prepend("^");
+// console.log(builder.value); // '^.^'
+
+// builder.pad("=");
+// console.log(builder.value); // '=^.^='
+// ------------------------------------------------------#5---------------------------------------
+
+// class Car {
+//   static getSpecs(car) {
+//     console.log(
+//       `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`
+//     );
+//   }
+
+//   constructor(obj) {
+//     this.speed = 0;
+//     this.maxSpeed = obj.maxSpeed;
+//     this._price = obj.price;
+//     this.isOn = false;
+//     this.distance = 0;
+//   }
+
+//   get price() {
+//     return this._price;
+//   }
+
+//   set price(newPrice) {
+//     this._price = newPrice;
+//   }
+
+//   turnOn() {
+//     this.isOn = true;
+//   }
+
+//   turnOff() {
+//     this.isOn = false;
+//     this.speed = 0;
+//   }
+
+//   accelerate(value) {
+//     if (this.maxSpeed >= this.speed + value) {
+//       this.speed = this.speed + value;
+//     }
+//   }
+
+//   decelerate(value) {
+//     if (this.speed - value >= 0) {
+//       this.speed = this.speed - value;
+//     }
+//   }
+
+//   drive(hours) {
+//     if (this.isOn === true) {
+//       this.distance += hours * this.speed;
+//     }
+//   }
+// }
+
+// const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+// mustang.turnOn();
+// mustang.accelerate(50);
+// mustang.drive(2);
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+// mustang.decelerate(20);
+// mustang.drive(1);
+// mustang.turnOff();
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+// console.log(mustang.price); // 2000
+// mustang.price = 4000;
+// console.log(mustang.price); // 4000
